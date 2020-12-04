3eb74617d1541ffef818550953f77fdd584aff97
Vladyslav Bobrykov <vladislaw.bobrikov@gmail.com>
2020-12-04 13:31:12 +0200

    lab03: added team versus team battle method

--- lab03/src/battle/Battle.java
+++ lab03/src/battle/Battle.java
@@ -32,7 +32,8 @@ public class Battle {
     public void oneVersusOne(Droid firstDroid, Droid secondDroid) {
         boolean firstMove = random.nextBoolean();
         System.out.printf("\r\nIn battle take part 2 droids:\r\n%s have %d HP and %d max damage\r\n%s have %d HP and %d max damage\r\n\r\n",
-        firstDroid.getName(), firstDroid.getHealth(), firstDroid.getDamage(), secondDroid.getName(), secondDroid.getHealth(), secondDroid.getDamage());
+        firstDroid.getName(), firstDroid.getHealth(), firstDroid.getDamage(),
+                secondDroid.getName(), secondDroid.getHealth(), secondDroid.getDamage());
         for(int move = 0; ; move++, firstMove = !firstMove) {
             if(firstDroid.getHealth() <= 0) {
                 System.out.printf("%s won\r\n", secondDroid.getName());
@@ -44,11 +45,13 @@ public class Battle {
             if (firstMove == true) {
                 firstDroid.toDamage(secondDroid);
                 System.out.printf("%s droid attacked %s droid\r\n" +
-                        "%s have %d HP and %s have %d HP\r\n\r\n",firstDroid.getName(), secondDroid.getName(),  firstDroid.getName(), firstDroid.getHealth(), secondDroid.getName(), secondDroid.getHealth());
+                        "%s have %d HP and %s have %d HP\r\n\r\n",firstDroid.getName(), secondDroid.getName(),
+                        firstDroid.getName(), firstDroid.getHealth(), secondDroid.getName(), secondDroid.getHealth());
             } else {
                 secondDroid.toDamage(firstDroid);
                 System.out.printf("%s droid attacked %s droid\r\n" +
-                        "%s have %d HP and %s have %d HP\r\n\r\n",secondDroid.getName(), firstDroid.getName(), firstDroid.getName(), firstDroid.getHealth(), secondDroid.getName(), secondDroid.getHealth());
+                        "%s have %d HP and %s have %d HP\r\n\r\n",secondDroid.getName(), firstDroid.getName(),
+                        firstDroid.getName(), firstDroid.getHealth(), secondDroid.getName(), secondDroid.getHealth());
             }
         }
     }
@@ -63,10 +66,8 @@ public class Battle {
         int memberFromSecondTeam = 0;
         int deathFromFirstTeam = 0;
         int deathFromSecondTeam = 0;
-        boolean firstEntryLoop1 = true;
-        boolean firstEntryLoop2 = true;
 
-        System.out.printf("In battle take part 2 teams\r\n" +
+        System.out.printf("\r\n\r\nIn battle take part 2 teams\r\n" +
                 "In the first team will play\r\n");
         for(int member = 0; member < amountDroids/2; member++) {
             firstTeam.add(droids.get(member));
@@ -74,61 +75,78 @@ public class Battle {
                     firstTeam.get(member).getHealth(), firstTeam.get(member).getDamage());
         }
 
-        System.out.printf("In the second team will play\r\n");
-        for(int member = 4; member < amountDroids; member++) {
-            secondTeam.add(droids.get(member));
+        System.out.printf("\r\nIn the second team will play\r\n");
+        for(int member = 0; member + amountDroids/2 < amountDroids; member++) {
+            secondTeam.add(droids.get(member + amountDroids/2));
             System.out.printf("Droid %s he has %d HP and %d max damage\r\n", secondTeam.get(member).getName(),
                     secondTeam.get(member).getHealth(), secondTeam.get(member).getDamage());
         }
 
-
-
         for(int move = 0; ; move++, firstMove = !firstMove) {
+
             for(int member = 0; member < amountDroids/2 - deathFromFirstTeam; member++) {
-                if(deathFromFirstTeam == amountDroids/2) {
-                    breakLoop = true;
-                    break;
-                }
-                if(firstTeam.get(member).getHealth() <=0 ) {
+                if(firstTeam.get(member).getHealth() <= 0 ) {
                     deathFromFirstTeam++;
+                    System.out.printf("\r\nDroid %s from the first team is dead\r\n", firstTeam.get(member).getName());
                     firstTeam.remove(member);
                     break;
                 }
             }
-            if(breakLoop == true) {
-                breakLoop = false;
-                break;
-            }
 
             for(int member = 0; member < amountDroids/2 - deathFromFirstTeam; member++) {
-                if(deathFromSecondTeam == amountDroids/2) {
-                    breakLoop = true;
-                    break;
-                }
-                if(secondTeam.get(member).getHealth() <=0 ) {
+                if(secondTeam.get(member).getHealth() <= 0 ) {
                     deathFromSecondTeam++;
+                    System.out.printf("\r\nDroid %s from the second team is dead\r\n", secondTeam.get(member).getName());
                     secondTeam.remove(member);
                     break;
                 }
             }
-            if(breakLoop == true) {
-                breakLoop = false;
-                break;
-            }
 
             if(firstMove == true) {
+                if(amountDroids/2 == deathFromFirstTeam) {
+                    System.out.printf("\r\nThe second team won\r\n");
+                    return;
+                }
+                if(deathFromSecondTeam == amountDroids/2) {
+                    System.out.printf("\r\nThe first team won\r\n");
+                    return;
+                }
                 memberFromFirstTeam = random.nextInt(amountDroids/2 - deathFromFirstTeam);
                 memberFromSecondTeam = random.nextInt(amountDroids/2 - deathFromSecondTeam);
                 firstTeam.get(memberFromFirstTeam).toDamage(secondTeam.get(memberFromSecondTeam));
+                System.out.printf("\r\nDroid from the first team %s attacked droid from the second team %s \r\n",
+                        firstTeam.get(memberFromFirstTeam).getName(), secondTeam.get(memberFromSecondTeam).getName());
+                System.out.printf("\r\nThe first team:\r\n");
+                for(int member = 0; member < amountDroids/2 - deathFromFirstTeam; member++) {
+                    System.out.printf("%s has %d HP\r\n", firstTeam.get(member).getName(), firstTeam.get(member).getHealth());
+                }
+                System.out.printf("\r\nThe second team:\r\n");
+                for(int member = 0; member < amountDroids/2 - deathFromSecondTeam; member++) {
+                    System.out.printf("%s has %d HP\r\n", secondTeam.get(member).getName(), secondTeam.get(member).getHealth());
+                }
             } else {
+                if(amountDroids/2 == deathFromFirstTeam) {
+                    System.out.printf("\r\nThe second team won\r\n");
+                    return;
+                }
+                if(deathFromSecondTeam == amountDroids/2) {
+                    System.out.printf("\r\nThe first team won\r\n");
+                    return;
+                }
                 memberFromFirstTeam = random.nextInt(amountDroids/2 - deathFromFirstTeam);
                 memberFromSecondTeam = random.nextInt(amountDroids/2 - deathFromSecondTeam);
                 secondTeam.get(memberFromSecondTeam).toDamage(firstTeam.get(memberFromFirstTeam));
+                System.out.printf("\r\nDroid from the second team %s attacked droid from the first team %s \r\n",
+                         secondTeam.get(memberFromSecondTeam).getName(), firstTeam.get(memberFromFirstTeam).getName());
+                System.out.printf("\r\nThe second team:\r\n");
+                for(int member = 0; member < amountDroids/2 - deathFromSecondTeam; member++) {
+                    System.out.printf("%s has %d HP\r\n", secondTeam.get(member).getName(), secondTeam.get(member).getHealth());
+                }
+                System.out.printf("\r\nThe first team:\r\n");
+                for(int member = 0; member < amountDroids/2 - deathFromFirstTeam; member++) {
+                    System.out.printf("%s has %d HP\r\n", firstTeam.get(member).getName(), firstTeam.get(member).getHealth());
+                }
             }
         }
-
     }
-
-
-
 }
--- lab03/src/com/company/Main.java
+++ lab03/src/com/company/Main.java
@@ -9,10 +9,11 @@ public class Main {
     static private final ArrayList<Droid> droids = new ArrayList<>();
 
     public static void main(String[] args) {
-	    Battle battle = new Battle(droids, 2);
+	    Battle battle = new Battle(droids, 4);
 
 
-	    battle.oneVersusOne(droids.get(0), droids.get(1));
+	    //battle.oneVersusOne(droids.get(0), droids.get(1));
+        battle.teamVersusTeam(droids);
 
     }
 }
